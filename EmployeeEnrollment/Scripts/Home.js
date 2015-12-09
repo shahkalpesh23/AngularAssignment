9aece52f1bb54df6f2db394a68919f62e56ec594
Kalpesh Shah <kshah@tavisca.com>
2015-12-09 13:13:50 +0530

    Restructured angular modules according to pages

---
+++ EmployeeEnrollment/Scripts/Home.js
@@ -0,0 +1,51 @@
+﻿
+app.controller("EmpController", function ($scope, EnrollmentServices, AuthenticateUser, IsAuthenticated, $location) {
+
+    $scope.Employee = {};
+    $scope.Employee.Salary = 1000;
+    $scope.IsItemEditable = false;
+    $scope.EmployeeList = [];
+
+    $scope.AcceptEnrollment = function () {
+        if ($scope.empForm.$invalid) return false;
+
+        EnrollmentServices.EnrollEmployee($scope.Employee);
+        $scope.EmployeeList = EnrollmentServices.GetEnrolledEmployees();
+        clearInputs();
+    };
+
+    $scope.EditItem = function (index) {
+        $scope.IsItemEditable = true;
+        $scope.Employee = EnrollmentServices.EditEnrollment(index);
+    };
+
+    $scope.UpdateEnrollment = function () {
+        if (!$scope.empForm.$invalid) {
+            EnrollmentServices.UpdateEnrollment($scope.Employee);
+            $scope.EmployeeList = EnrollmentServices.GetEnrolledEmployees();
+            clearInputs();
+        }
+    };
+
+    $scope.RemoveEnrollment = function (empObj) {
+        EnrollmentServices.DeleteEnrollment(empObj);
+        $scope.EmployeeList = EnrollmentServices.GetEnrolledEmployees();
+        clearInputs();
+    };
+
+    $scope.SignOut = function () {
+        IsAuthenticated.success = AuthenticateUser.SignOut()
+    }
+
+    function clearInputs() {
+        $scope.IsItemEditable = false;
+        $scope.Employee = null;
+    }
+
+    (function init() {
+        if (!IsAuthenticated.success) {
+            $location.path("/login");
+        }
+    })();
+});
+
--- EmployeeEnrollment/Scripts/Login.js
+++ EmployeeEnrollment/Scripts/Login.js
@@ -1,9 +1,4 @@
 ﻿
-var app = angular.module("empApp", ["ngRoute", "empApp.Directives.EmployeeDirective", "empApp.Services.EmployeeServices"]);
-app.constant("UserName", "Admin");
-app.constant("Password", "Admin@123");
-app.value("IsAuthenticated", { success: false });
-
 app.controller("loginController", function (AuthenticateUser, EnrollmentServices, IsAuthenticated) {
 
     var that = this;
@@ -21,55 +16,3 @@ app.controller("loginController", function (AuthenticateUser, EnrollmentServices
     })();
 });
 
-app.provider("AuthenticateUser", function (UserName, Password) {
-
-    var that = this;
-    that.locationObj = null;
-
-    that.GetNavigationObject = function (locObj) {
-        if (locObj) {
-            that.locationObj = locObj;
-        }
-    }
-
-    that.SignIn = function (creds) {
-        if (creds.UserName === UserName && creds.Password === Password) {
-            that.locationObj.path("/home")
-            return true;
-        }
-        that.locationObj.path("/login");
-        return false;
-    }
-
-    that.SignOut = function () {
-        that.locationObj.path("/login");
-        return false;
-    }
-
-    that.$get = function () {
-        return that;
-    }
-
-});
-
-app.config(["$routeProvider", function ($routeProvider) {
-
-    $routeProvider
-        .when("/login", {
-            templateUrl: "/Views/Login.html",
-            controller: "loginController",
-            controllerAs: "login"
-        }).when("/home", {
-            templateUrl: "/Views/Home.htm",
-            controller: "EmpController"
-        }).otherwise({
-            redirectTo: "/login"
-        });
-}]);
-
-app.run(function ($location, AuthenticateUser) {
-
-    AuthenticateUser.GetNavigationObject($location);
-    $location.path("/login");
-
-});
